0873500dc1419c41298dc0ac0fb081b3d2b8fab4
AlbaLilium <flos.alba.lilium@gmail.com>
2024-10-28 00:52:15 +0300

    fix: add validation for numeric values in product model

--- app/api/v1/dependecies/product_validator.py
+++ app/api/v1/dependecies/product_validator.py
@@ -1,51 +1,66 @@
-from decimal import Decimal, ROUND_HALF_UP
-from fastapi import  HTTPException
+from decimal import ROUND_HALF_UP, Decimal
 
-def validate_amount(amount: int)-> int:
+from fastapi import HTTPException
+
+
+def validate_amount(amount: int) -> int:
     """
     Check cost to be 0< .
-    Raise 400 if it's lower than 0.
 
     :param amount: int
+    :raise HTTPException 400
+        if amount is lower than 0.
     :return: int
     """
     if amount < 0:
-        raise HTTPException(
-            status_code=400, detail="Incorrect amount."
-        )
+        raise HTTPException(status_code=400, detail="Incorrect amount.")
     return amount
 
-def validate_cost(cost: Decimal)->Decimal:
+
+def validate_cost(cost: Decimal) -> Decimal:
     """
     Check cost to be 0< .
-    Raise 400 if it's lower than 0.
-
 
     :param cost: Decimal
+
+    :raise HTTPException 400
+     if cost lower than 0
+
     :return: decimal in template {0.00}
     """
 
     if cost < 0:
-        raise  HTTPException(
-            status_code=400, detail= "Incorrect cost. "
-        )
+        raise HTTPException(status_code=400, detail="Incorrect cost. ")
     cents = Decimal("0.01")
 
     return cost.quantize(cents, ROUND_HALF_UP)
 
 
-
-def validate_text(text:str)->str:
+def validate_text(text: str) -> str:
     """
      Check string on being empty.
 
-     Raise 400 if it is empty.
-
     :param text: str
+    :raise HTTPException 400 if it is empty.
     :return: str
     """
-    if text == '':
-        raise HTTPException(
-            status_code=400, detail= "Field shouldn't be empty"
-            )
+    if text == "":
+        raise HTTPException(status_code=400, detail="Field shouldn't be empty")
     return text
+
+
+def validate_numeric_values(cost: Decimal, amount: int)-> bool:
+    """
+
+    :param cost: int
+
+    :param amount: int
+
+    :raise HTTPException
+    :return: bool
+    """
+    if cost:
+        cost = validate_cost(cost)
+    elif amount:
+        amount = validate_amount(amount)
+    return True
